6c540ab0fb005deff9eedc1bdc72c7d47e17f16e
tacoRiot <46939881+tacoRiot@users.noreply.github.com>
2019-07-26 12:38:49 -0600

    had to replace lost code

---
+++ Mugshot/Scientist.swift
@@ -0,0 +1,18 @@
+//
+//  Scientist.swift
+//  Mugshot
+//
+//  Created by taco on 7/26/19.
+//  Copyright © 2019 tacoTruck. All rights reserved.
+//
+
+import Foundation
+
+struct Scientist: Decodable {
+    let name: String
+    let dates: String
+    let field: String
+    let bio: String
+    let country: String
+    let source: String
+}
--- Mugshot/ViewController.swift
+++ Mugshot/ViewController.swift
@@ -13,12 +13,14 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var scientists = [String: Scientist]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
+        loadData()
         
     }
     
@@ -26,7 +28,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        
+        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "scientists", bundle: nil) else {
+            fatalError("Couldn't load tracking images")
+        }
+        
+        configuration.trackingImages = trackingImages
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -37,5 +45,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    func loadData() {
+        guard let url = Bundle.main.url(forResource: "scientists", withExtension: "json") else {
+            fatalError("Unable to find JSON in bundle")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Unable to load JSON")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loadedScientists = try? decoder.decode([String: Scientist].self, from: data) else {
+            fatalError("Unable to parse JSON.")
+        }
+        
+        scientists = loadedScientists
+    }
+    
+    func textNode(_ str: String, font: UIFont, maxWidth: Int? = nil) -> SCNNode {
+        let text = SCNText(string: str, extrusionDepth: 0)
+        
+        text.flatness = 0.1
+        text.font = font
+        
+        if let maxWidth = maxWidth {
+            text.containerFrame = CGRect(origin: .zero, size: CGSize(width: maxWidth, height: 500))
+            text.isWrapped = true
+        }
+        
+        let textNode = SCNNode(geometry: text)
+        textNode.scale = SCNVector3(0.002, 0.002, 0.002)
+        
+        return textNode
     }
 }
